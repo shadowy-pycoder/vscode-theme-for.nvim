a1a72c3359a6b11ba49bb54e05f813624d008540
shadowy-pycoder <35629483+shadowy-pycoder@users.noreply.github.com>
2026-04-13 16:26:34 +0300

    match f triple quotes

--- queries/python/highlights.scm
+++ queries/python/highlights.scm
@@ -5,5 +5,11 @@
  (#match? @string.prefix "^[fFrRbBtT]{1,2}['\"]$")
  (#offset! @string.prefix 0 0 0 -1))
 
+((string
+   (string_start) @string.prefix)
+ (#match? @string.prefix "^[fFrRbBtT]{1,2}['\"]{3}$")
+ (#offset! @string.prefix 0 0 0 -3))
+
 ((function_definition
    "async" @async_func_keyword))
+
